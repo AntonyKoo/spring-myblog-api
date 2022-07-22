604cd4c30b6472d1c88e58087f3bc935c9cd5c7b
Antony Koo <kina5418@naver.com>
2022-07-22 16:34:14 +0900

    Initial commit

---
+++ src/test/java/com/sparta/myblogsparta/MyblogSpartaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.myblogsparta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyblogSpartaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
